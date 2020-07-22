d2c05fa35b36ebe8429765fbd05843ae2c87fbc6
Daniel Beckley <tomiwabeckley@gmail.com>
2020-07-22 16:42:46 +0100

    Updated state with Redux

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflow:'scroll', border:'2px solid black', maxHeight:'450px'}}>
+        <div style={{overflow:'scroll', border:'2px solid black', height:'500px'}}>
             {props.children}
         </div>
     );
